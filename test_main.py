c2275f3d91b2caba1b1b3ac2abb63b64e57fd47a
Nikhit Adusumilli <nikhitadusumilli@gmail.com>
2025-09-10 13:59:43 +0000

    Adding fixtures to the test.

--- test_main.py
+++ test_main.py
@@ -1,6 +1,12 @@
+import pytest
 from main import StringCalculator
 
 
-def test_add():
-    string_calculator = StringCalculator()
+@pytest.fixture
+def string_calculator():
+    """Creates a fresh instance of the String Calculator before each test."""
+    return StringCalculator()
+
+
+def test_add_strings(string_calculator):
     assert string_calculator.Add
