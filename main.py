8f4a705b7dd538288daea682959d56062568a4a3
Nikhit Adusumilli <nikhitadusumilli@gmail.com>
2025-09-10 14:37:55 +0000

    Adding test cases for single number and non string instances and also functionality for the same.

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 
 class StringCalculator:
+    """
+        String Calculator, performs mathematical calculations on strings.
+    """
+
     def __init__(self):
         pass
 
-    def Add(self, str):
-        if not str:
+    def Add(self, operands):
+        """
+            Takes a numbers separated by comma as a string and performs mathematical 
+            addition on them and returns the result.
+        """
+        # If its not a string raise value error.
+        if not isinstance(operands, str):
+            print("\n\n\n\n", operands)
+            raise ValueError(
+                "The operands have to be numbers separate by comma.")
+
+        # If it is empty string or none return 0.
+        if not operands:
             return 0
+
+        # If it is empty string or none return 0.
+        if "," not in operands:
+            return int(operands)
--- test_main.py
+++ test_main.py
@@ -4,14 +4,48 @@ from main import StringCalculator
 
 @pytest.fixture
 def string_calculator():
-    """Creates a fresh instance of the String Calculator before each test."""
+    """
+        Creates a fresh instance of the String Calculator before each test.
+    """
     return StringCalculator()
 
 
 @pytest.mark.parametrize("str,expected", [
     (None, 0),
+    (90, 0),
+    ([], 0),
+])
+def test_add_invalid_params(string_calculator, str, expected):
+    """
+        Checks the validity of the opperands to see if an error is raised
+    """
+    with pytest.raises(ValueError, match="The operands have to be numbers separate by comma."):
+        string_calculator.Add(str)
+
+
+@pytest.mark.parametrize("str,expected", [
     ("", 0),
 ])
 def test_add_empty_strings(string_calculator, str, expected):
+    """
+        Creates a fresh instance of the String Calculator before each test.
+    """
     assert string_calculator.Add(
         str) == expected, f'Adding {str} should give {expected} as a result.'
+
+
+@pytest.mark.parametrize("str,expected", [
+    ("1", 1),
+    ("2", 2),
+    ("20", 20),
+    ("50", 50),
+    ("100", 100),
+    ("100000000", 100000000),
+])
+def test_add_one_string(string_calculator, str, expected):
+    """
+        Creates a fresh instance of the String Calculator before each test.
+    """
+    total = string_calculator.Add(str)
+    assert isinstance(total, int)
+    assert total == expected
